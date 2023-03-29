2ac71c2d113d24f019478ab00106c38d567ab18c
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-03-29 09:56:37 +0100

    deletes a tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,34 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * free_tree - deletes a node
- * tree: pointer to node to delete
- */
-
-void free_tree(binary_tree_t *tree)
-{
-	if (!tree)
-		return;
-
-	free(tree->n);
-	free(tree->left);
-	free(tree->right);
-	free(tree->parent);
-	free(tree);
-}
-
-
-
-/**
  * binary_tree_delete - deletes an entire binary tree
- * tree: pointer to thr root node of the tree to delete
+ * @tree: pointer to thr root node of the tree to delete
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
-		return;
-
-	tree->left ? binary_tree_delete(tree->left): free_tree(tree);
-	tree->right ? binary_tree_delete(tree->right): free_tree(tree);
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
